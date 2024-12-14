d9db517f8d4d8ffb1256e4e771e6e18f84c5f8e1
devfaisalkhan <dev.faisalkhan@gmail.com>
2024-12-14 16:02:06 +0500

    Fix: Resolve sidebar item page refresh issue and implement treeview initialization in AdminLTE

--- web/src/app/admin/components/main-sidebar/main-sidebar.component.ts
+++ web/src/app/admin/components/main-sidebar/main-sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-main-sidebar',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './main-sidebar.component.html',
   styleUrl: './main-sidebar.component.scss'
 })
-export class MainSidebarComponent {
+export class MainSidebarComponent implements AfterViewInit{
+  
+  ngAfterViewInit(): void {
+    
+    $(document).ready(function () {
+      $('[data-widget="treeview"]').Treeview('init');
+    });
+  }
 
 }
