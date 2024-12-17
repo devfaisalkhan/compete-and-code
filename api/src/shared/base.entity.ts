7a13efbc4516b46b0b7a31206ff885520b503acc
devfaisalkhan <dev.faisalkhan@gmail.com>
2024-12-17 21:30:36 +0500

    chore: add timestamps for entries

--- api/src/shared/base.entity.ts
+++ api/src/shared/base.entity.ts
@@ -1,8 +1,17 @@
 import { UUID } from "crypto";
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
 
 @Entity()
-export class Base {
-    @PrimaryGeneratedColumn('uuid')
-    id: UUID
+export abstract class Base {
+  @PrimaryGeneratedColumn('uuid')
+  id: UUID; 
+
+  @CreateDateColumn()
+  createdAt: Date; 
+
+  @UpdateDateColumn()
+  updatedAt: Date; 
+
+  @DeleteDateColumn({ nullable: true })
+  deletedAt?: Date;
 }
